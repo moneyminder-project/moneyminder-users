4cec6fe982351e9bbe123ac6628c2ac68d71119b
saul <smartinezpad@uoc.edu>
2025-04-01 18:58:34 +0200

    Initial commit

---
+++ src/main/java/com/moneyminder/moneyminderusers/MoneyMinderUsersApplication.java
@@ -0,0 +1,13 @@
+package com.moneyminder.moneyminderusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMinderUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyMinderUsersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moneyminder/moneyminderusers/MoneyMinderUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneyminder.moneyminderusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMinderUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
